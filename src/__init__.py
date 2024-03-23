76b83c275ef2c891a78d65c97d855c24aa1d96b3
chip <a917kk@mail.ru>
2024-03-23 18:11:12 +0300

    add CORS

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from dynaconf import FlaskDynaconf
 from flasgger import Swagger
+from flask_cors import CORS
 
 app = Flask(__name__, template_folder="../templates")
+CORS(app, supports_credentials=True)
+
 flask_conf = FlaskDynaconf(
     app,
     settings_files=["settings.yaml", ".secrets.yaml"],
